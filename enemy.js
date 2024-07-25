f4298829eccdc73404bb678af557ce293c3619db
dellmonitor <123smorodina@gmail.com>
2024-07-25 16:02:05 +0300

    add move method to enemy

--- enemy.js
+++ enemy.js
@@ -4,8 +4,15 @@ function Enemy(x, y, hitbox, color) {
 
 Enemy.prototype = Object.create(Entity.prototype);
 
+Enemy.prototype.move = function() {
+	this.x += (Math.round(Math.random()) * 2 - 1);
+	this.y += (Math.round(Math.random()) * 2 - 1);
+}
+
 Enemy.prototype.update = function() {
+	this.move();
 	this.color = 'red';
+	this.hitbox = new Rectangle(this.x, this.x, 16, 16);
 	if (this.isColliding) {
 		this.color = 'pink';
 	}
