40248bf0aece78a240788dcab59220397d1c7278
dellmonitor <123smorodina@gmail.com>
2024-07-25 14:10:37 +0300

    add collision detection to enemy

--- enemy.js
+++ enemy.js
@@ -5,4 +5,8 @@ function Enemy(x, y, hitbox, color) {
 Enemy.prototype = Object.create(Entity.prototype);
 
 Enemy.prototype.update = function() {
+	this.color = 'red';
+	if (this.isColliding) {
+		this.color = 'pink';
+	}
 }
