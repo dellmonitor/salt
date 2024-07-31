61fe7c95bcf3907e262b10b70606fd965e0c0a5f
dellmonitor <123smorodina@gmail.com>
2024-07-31 18:12:35 +0300

    wrap rectangle as class

--- rectangle.js
+++ rectangle.js
@@ -1,26 +1,27 @@
-Rectangle = function(x, y, width, height) {
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
+class Rectangle {
+	constructor(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 
-	this.left = x - width;
-	this.right = x + width;
-	this.top = y - height;
-	this.bottom = y + height;
-};
+		this.left = x - width;
+		this.right = x + width;
+		this.top = y - height;
+		this.bottom = y + height;
+	}
 
-Rectangle.prototype.contains = function(point) {
-	return (point.x >= this.left 	&&
-			point.x <= this.right	&&
-			point.y >= this.top		&&
-			point.y <= this.bottom);
-};
+	contains(point) {
+		return (point.x >= this.left 	&&
+				point.x <= this.right	&&
+				point.y >= this.top		&&
+				point.y <= this.bottom);
+	}
 
-Rectangle.prototype.intersects = function(range) {
-	return ! (range.x - range.width > this.x + this.width 	||
-			  range.x + range.width < this.x - this.width 	||
-			  range.y - range.height > this.y + this.height ||
-			  range.y + range.height < this.y - this.height);
+	intersects(range) {
+		return ! (range.x - range.width > this.x + this.width 	||
+				  range.x + range.width < this.x - this.width 	||
+				  range.y - range.height > this.y + this.height ||
+				  range.y + range.height < this.y - this.height);
+	}
 }
-
