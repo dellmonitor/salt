d3d47c347e7dceacdc6a16ba81ffeac26c288c9a
dellmonitor <123smorodina@gmail.com>
2024-07-31 18:10:04 +0300

    wrap quadtree as class

--- quadtree.js
+++ quadtree.js
@@ -1,79 +1,82 @@
-Point = function(x, y, data) {
-	this.x = x;
-	this.y = y;
-	this.userData = data;
-};
+class Point {
+	constructor(x, y, data) {
+		this.x = x;
+		this.y = y;
+		this.userData = data;
+	}
+}
 
-Quadtree = function(boundary, n) {
-	this.boundary = boundary;
-	this.capacity = n;
-	this.isDivided = false;
-	this.points =[];
-};
-
-Quadtree.prototype.insert = function(point) {
-	if (! this.boundary.contains(point)) {
-		return false;
+class Quadtree {
+	constructor(boundary, n) {
+		this.boundary = boundary;
+		this.capacity = n;
+		this.isDivided = false;
+		this.points =[];
 	}
 
-	if (this.points.length < this.capacity) {
-		this.points.push(point);
-		return true;
-	} else {
-		if (! this.isDivided) {
-			this.subdivide();
+	subdivide() {
+		const x = this.boundary.x
+		const y = this.boundary.y
+		const width = this.boundary.width / 2;
+		const height = this.boundary.height / 2;
+
+		let ne = new Rectangle(x + width, y - height, width, height);
+		this.northeast = new Quadtree(ne, this.capacity);
+
+		let se = new Rectangle(x + width, y + height, width, height);
+		this.southeast = new Quadtree(se, this.capacity);
+
+		let sw = new Rectangle(x - width, y + height, width, height);
+		this.southwest = new Quadtree(sw, this.capacity);
+
+		let nw = new Rectangle(x - width, y - height, width, height);
+		this.northwest = new Quadtree(nw, this.capacity);
+
+		this.isDivided = true;
+	}
+
+	insert(point) {
+		if (! this.boundary.contains(point)) {
+			return false;
 		}
-		this.northeast.insert(point) ||
-		this.southeast.insert(point) ||
-		this.southwest.insert(point) ||
-		this.northwest.insert(point);
-	}
-};
 
-Quadtree.prototype.subdivide = function() {
-	const x = this.boundary.x
-	const y = this.boundary.y
-	const width = this.boundary.width / 2;
-	const height = this.boundary.height / 2;
-
-	let ne = new Rectangle(x + width, y - height, width, height);
-	this.northeast = new Quadtree(ne, this.capacity);
-
-	let se = new Rectangle(x + width, y + height, width, height);
-	this.southeast = new Quadtree(se, this.capacity);
-
-	let sw = new Rectangle(x - width, y + height, width, height);
-	this.southwest = new Quadtree(sw, this.capacity);
-
-	let nw = new Rectangle(x - width, y - height, width, height);
-	this.northwest = new Quadtree(nw, this.capacity);
-
-	this.isDivided = true;
-};
-
-Quadtree.prototype.query = function(range, found) {
-	if (! found) {
-		found = [];
-	}
-
-	if (! range.intersects(this.boundary)) {
-		return found;
-	}
-
-	for (const p of this.points) {
-		if (range.contains(p)) {
-			found.push(p);
+		if (this.points.length < this.capacity) {
+			this.points.push(point);
+			return true;
+		} else {
+			if (! this.isDivided) {
+				this.subdivide();
+			}
+			this.northeast.insert(point) ||
+			this.southeast.insert(point) ||
+			this.southwest.insert(point) ||
+			this.northwest.insert(point);
 		}
 	}
 
-	if (this.isDivided) {
-		this.northeast.query(range, found);
-		this.southeast.query(range, found);
-		this.southwest.query(range, found);
-		this.northwest.query(range, found);
+	query(range, found) {
+		if (! found) {
+			found = [];
+		}
+
+		if (! range.intersects(this.boundary)) {
+			return found;
+		}
+
+		for (const p of this.points) {
+			if (range.contains(p)) {
+				found.push(p);
+			}
+		}
+
+		if (this.isDivided) {
+			this.northeast.query(range, found);
+			this.southeast.query(range, found);
+			this.southwest.query(range, found);
+			this.northwest.query(range, found);
+			return found;
+		}
+
 		return found;
 	}
-
-
-	return found;
-};
+}
