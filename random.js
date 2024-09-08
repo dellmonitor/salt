84e4c6706935207d4fad956dcd1130e0dfb2ebb8
dellmonitor <123smorodina@gmail.com>
2024-09-08 23:42:29 +0300

    clear up random.js

--- random.js
+++ random.js
@@ -11,11 +11,3 @@ function xoshiro128ss(a, b, c, d) {
     return (r >>> 0) / 4294967296;
   }
 }
-
-const seedgen = () => (Math.random()*2**32)>>>0;
-// const getRand = xoshiro128ss(seedgen(), seedgen(), seedgen(), seedgen());
-
-const getRandNE = xoshiro128ss(seedgen(), 0, 0, 0);
-const getRandNW = xoshiro128ss(0, seedgen(), 0, 0);
-const getRandSW = xoshiro128ss(0, 0, seedgen(), 0);
-const getRandSE = xoshiro128ss(0, 0, 0, seedgen(), );
